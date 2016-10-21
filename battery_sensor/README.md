0108434a5b0c590026a30b9c5441343417e40791
Anais <anaisreynaud@hotmail.fr>
2016-10-21 16:01:28 +0200

    updated arduino program

---
+++ battery_sensor/README.md
@@ -0,0 +1,25 @@
+battery_sensor
+
+ROS packages related to read the sensor values (voltage and current) using the arduino and send them in a ROS message
+
+The following ROS packages are part of this repo:
+
+- battery_sensor_arduino contains the program that has to be flashed to the Arduino. 
+
+- battery_sensor_msgs contains the custom message for sending the values of the measures.
+
+
+To flash the program on the Arduino:
+-> cd ~/thor/build/battery_sensor_arduino
+-> make battery_sensor_arduino_firmware_sensor-upload
+
+
+To launch the communication with the Arduino:
+-> roscore
+-> rosrun rosserial_python serial_node.py /dev/ttyUSB0
+(-> rostopic echo measures)
+-> rqt_plot measures/voltage
+
+
+To print the serial messages comming from the Arduino:
+-> screen /dev/ttyUSB0 9600
---
+++ battery_sensor/battery_sensor_arduino/firmware/program.cpp
@@ -0,0 +1,65 @@
+/*
+* read the sensor values (voltage and current) and send them in a ros message 
+*/
+
+#include <ros.h>
+#include <battery_sensor_msgs/Measures.h>
+#include <Sensor.h>
+
+#include <Arduino.h>
+
+
+Sensor S1 = Sensor(0,1);
+
+ros::NodeHandle nh;
+
+battery_sensor_msgs::Measures measures_msg_1;
+ros::Publisher pub("measures", &measures_msg_1);
+
+/*
+int freeRam()
+{
+  extern int __heap_start, *__brkval;
+  int v;
+  return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);
+}
+*/
+
+void setup()
+{
+  nh.initNode();
+  nh.advertise(pub);
+
+  //Serial.begin(9600);
+}
+
+
+long msg_delay = 100;
+long new_millis;
+long last_millis = millis();
+
+
+void loop()
+{  
+  new_millis = millis();
+  
+  if( (new_millis - last_millis) > msg_delay)
+  {
+    S1.updateMeasures();
+    measures_msg_1.name = "battery1";
+    measures_msg_1.voltage = S1.getVoltage();
+    measures_msg_1.current = S1.getCurrent();
+    measures_msg_1.SOC = S1.getSOC();
+
+    pub.publish(&measures_msg_1);
+    nh.spinOnce();
+
+    /*
+    Serial.print("freeRam()=");
+    Serial.println(freeRam());
+    */
+
+    last_millis = new_millis;
+  }
+  
+}
